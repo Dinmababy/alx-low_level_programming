b12c1bdb74be95e1e8e81435ea529da808c5b4bb
Dinmababy <chidinmanzewodo@gmail.com>
2022-09-13 23:22:19 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
-* print_alphabets_x10(void)
-*
-* Return: 0
+*main - print_alphabets_x10
+*Description - print_alphabets_x10 print 10 times the alphabet, in lowercase
+*Return: 0
 */
+
 void print_alphabet_x10(void)
 {
 char c, i;
@@ -12,7 +14,7 @@ for (i = 0; i <= 9; i++)
 for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
-} 
+}
 _putchar('\n');
 }
 }
