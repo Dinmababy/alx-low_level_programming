7c20e64e4641bee4df923a0eec24eab49ddc2d82
Dinmababy <chidinmanzewodo@gmail.com>
2022-09-14 06:45:04 -0700

    task 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-*main - print_alphabets_x10
-*Description - print_alphabets_x10 print 10 times the alphabet, in lowercase
-*Return: 0
+*main-main block
+*Description:print -putchar in a new line
+*Return:this program returns zero
 */
 
 void print_alphabet_x10(void)
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-*main - _islower - function to check for lowercase charater
-*Descripition:@c is the int that will use for the argument of the function
-*Return:0 always
+*_islower - function to check for lowercase character
+*@c:  is the int that will use for the argument of the function
+*Return: 0
 */
 
 int _islower(int c)
