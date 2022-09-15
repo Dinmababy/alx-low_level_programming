369354d18803c0257ec67ad48faee03d20fd7a14
Dinmababy <chidinmanzewodo@gmail.com>
2022-09-15 15:46:18 -0700

    task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+#include <stdio.h>
+
+void postive_or_negative(int i);
+int largest number(int a, int b, int c);
+void print remaining days(int month, int day, int year);
+int convert day(int month, int day);
+#endif /*MAIN_H */
