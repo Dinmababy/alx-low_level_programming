f5a1e7a5b1bfeb88cc7001bb4c0a1f0bebbebd7d
Dinmababy <chidinmanzewodo@gmail.com>
2022-09-19 14:37:10 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,4 +7,15 @@
 * Return: the pointer to dest
 */
 
-char
+char *_strcpy(char *dest, char *src)
+{
+int i, length;
+for (length = 0; src[length] != '\0'; length++)
+{
+}
+for (i = 0; i <= length; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
