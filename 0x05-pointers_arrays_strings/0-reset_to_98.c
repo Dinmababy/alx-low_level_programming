e021973f5d80c54795552f9e18ac94cbd114855a
Dinmababy <chidinmanzewodo@gmail.com>
2022-09-19 16:25:14 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* reset_to_98 - set value of a variable to 98 using a pointer
+* @n: the address of the value needed to change
+* return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+n = 98;
+}
