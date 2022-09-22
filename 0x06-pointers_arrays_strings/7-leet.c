8a391abdc8c68e88e2a4cd05a3e9c5f2142480e3
Dinmababy <chidinmanzewodo@gmail.com>
2022-09-22 09:49:22 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* leet - encodes a string into 1337 .
+* @chi: string i used
+* Return: chi
+*/
+
+char *leet(char *chi)
+{
+int count = 0, i;
+
+int lower_case[] = {97, 101, 111, 116, 108};
+int upper_case[] = {65, 69, 79, 84, 76};
+int numbers[] = {52, 51, 48, 55, 49};
+
+while (*(chi + count) != '\0')
+
+{
+for (i = 0; i < 5; i++)
+{
+if (*(Night + count) == lower_case[i] || *(chi + count) == upper_case[i])
+{
+*(chi + count) = numbers[i];
+break;
+}
+}
+count++;
+}
+
+return (chi)
+
+}
