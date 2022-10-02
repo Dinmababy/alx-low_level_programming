9559aa98d93cddb436bdc4acb2745a195a7bffa0
Dinmababy <chidinmanzewodo@gmail.com>
2022-10-02 10:37:10 -0700

    task 1

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+*main - prints the multiplication of two integers
+*@argc: argument count
+*@argv: argument vector
+*Return: 0 if true, 1 if false
+*/
+
+int main(int argc, char *argv[])
+{
+int cass, chi;
+if (argc == 3)
+{
+cass = atoi(argv[1]);
+chi = atoi(argv[2]);
+
+printf("%d\n", night * burn);
+return (0);
+}
+printf("Error\n");
+return (1);
+}
